686644a9a07e6a2b368e0cbc884880d705960d32
blaine <blaine@stanford.edu>
2019-08-20 14:56:22 -0700

    Remove unused class member.

--- app/src/main/java/com/example/metrodrone/MidiDriverHelper.java
+++ app/src/main/java/com/example/metrodrone/MidiDriverHelper.java
@@ -7,9 +7,6 @@ import org.billthefarmer.mididriver.MidiDriver;
 
 public class MidiDriverHelper extends MidiDriver {
 
-    // Soundfont data
-    String soundfontFilename;
-
     // Convert BPM to ms per beat
     public static double getMsPerBeat(int bpm) {
         final long msPerMinute = 60 * 1000;
