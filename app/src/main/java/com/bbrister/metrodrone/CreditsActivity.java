b6d5752ee73a67537fe7842e30bb492968565a27
blaine <blaine@stanford.edu>
2019-11-01 01:12:32 -0700

    Display the correct build version in the credits page. Requires parsing the links from HTML, since Android's link parser doesn't seem to work when text is set dynamically.

--- app/src/main/java/com/bbrister/metrodrone/CreditsActivity.java
+++ app/src/main/java/com/bbrister/metrodrone/CreditsActivity.java
@@ -1,6 +1,7 @@
 package com.bbrister.metrodrone;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
@@ -15,8 +16,14 @@ public class CreditsActivity extends DroneActivity {
         // Set the content
         setContentLayout(R.layout.content_credits);
 
-        // Enable links in the textView
+        // Construct the text and parse the HTML links
         TextView textView = findViewById(R.id.creditsTextView);
+        textView.setText(Html.fromHtml(String.format(
+                getString(R.string.credits),
+                BuildConfig.VERSION_NAME
+        )));
+
+        // Enable links in the textView
         textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
